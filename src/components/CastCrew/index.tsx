3ce9694a1a60edf500e6294a4b05f752467b283b
João Volpato <volpatocode@gmail.com>
2022-05-13 00:43:13 -0300

    fix: cast & crew list

--- src/components/CastCrew/index.tsx
+++ src/components/CastCrew/index.tsx
@@ -5,13 +5,14 @@ import {
   ImageListItemStyle,
   ImageListStyled,
   ImageListItemsStyled,
+  TabPanelStyled,
+  TabStyled,
 } from "./styles";
 import { castCrewType } from "../../types/services";
-import Box from '@mui/material/Box';
-import Tab from '@mui/material/Tab';
-import TabContext from '@mui/lab/TabContext';
-import TabList from '@mui/lab/TabList';
-import TabPanel from '@mui/lab/TabPanel';
+import Box from "@mui/material/Box";
+import Tab from "@mui/material/Tab";
+import TabContext from "@mui/lab/TabContext";
+import TabList from "@mui/lab/TabList";
 
 export type propsType = {
   movie: any;
@@ -19,12 +20,12 @@ export type propsType = {
 
 export default function index({ movie }: propsType) {
   const [castCrew, setCastCrew] = useState({} as castCrewType);
-  const [value, setValue] = useState('1');
+  const [value, setValue] = useState("1");
 
   const handleChange = (event: React.SyntheticEvent, newValue: string) => {
     setValue(newValue);
   };
-  
+
   const API_IMG = "http://image.tmdb.org/t/p/original/";
 
   useEffect(() => {
@@ -39,65 +40,85 @@ export default function index({ movie }: propsType) {
   }, []);
 
   return (
-    <CastCrew movie={movie}>
-      <ImageListStyled gap={0} cols={1}>
-        {castCrew?.cast?.map((cast) => (
-          <ImageListItemsStyled key={cast.id}>
-            <img
-              src={
-                cast.profile_path
-                  ? API_IMG + cast.profile_path
-                  : "https://st3.depositphotos.com/23594922/31822/v/600/depositphotos_318221368-stock-illustration-missing-picture-page-for-website.jpg"
-              }
-              srcSet={
-                cast.profile_path
-                  ? API_IMG + cast.profile_path
-                  : "https://st3.depositphotos.com/23594922/31822/v/600/depositphotos_318221368-stock-illustration-missing-picture-page-for-website.jpg"
-              }
-              alt={cast.name ? cast.name : "No name provided"}
-              loading="lazy"
-            />
-            <ImageListItemStyle
-              title={cast.name ? cast.name : "No name provided"}
-              subtitle={
-                <span>
-                  as:{" "}
-                  {cast.character ? cast.character : "No character provided"}
-                </span>
-              }
-              position="below"
-            />
-          </ImageListItemsStyled>
-        ))}
-      </ImageListStyled>
-
-      <ImageListStyled gap={0} cols={1}>
-        {castCrew?.crew?.map((crew) => (
-          <ImageListItemsStyled key={crew.id}>
-            <img
-              src={
-                crew.profile_path
-                  ? API_IMG + crew.profile_path
-                  : "https://st3.depositphotos.com/23594922/31822/v/600/depositphotos_318221368-stock-illustration-missing-picture-page-for-website.jpg"
-              }
-              srcSet={
-                crew.profile_path
-                  ? API_IMG + crew.profile_path
-                  : "https://st3.depositphotos.com/23594922/31822/v/600/depositphotos_318221368-stock-illustration-missing-picture-page-for-website.jpg"
-              }
-              alt={crew.name ? crew.name : "No name provided"}
-              loading="lazy"
-            />
-            <ImageListItemStyle
-              title={crew.name ? crew.name : "No name provided"}
-              subtitle={
-                <span>as: {crew.job ? crew.job : "No job provided"}</span>
-              }
-              position="below"
-            />
-          </ImageListItemsStyled>
-        ))}
-      </ImageListStyled>
-    </CastCrew>
+      <CastCrew movie={movie}>
+        <Box sx={{ borderRadius: "5px" }}>
+          <TabContext value={value}>
+            <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+              <TabList
+                onChange={handleChange}
+                aria-label="lab API tabs example"
+              >
+                <TabStyled label="Cast" value="1" />
+                <TabStyled label="Crew" value="2" />
+              </TabList>
+            </Box>
+            <TabPanelStyled value="1">
+              <ImageListStyled gap={0} cols={1}>
+                {castCrew?.cast?.map((cast) => (
+                  <ImageListItemsStyled key={cast.id}>
+                    <img
+                      src={
+                        cast.profile_path
+                          ? API_IMG + cast.profile_path
+                          : "https://st3.depositphotos.com/23594922/31822/v/600/depositphotos_318221368-stock-illustration-missing-picture-page-for-website.jpg"
+                      }
+                      srcSet={
+                        cast.profile_path
+                          ? API_IMG + cast.profile_path
+                          : "https://st3.depositphotos.com/23594922/31822/v/600/depositphotos_318221368-stock-illustration-missing-picture-page-for-website.jpg"
+                      }
+                      alt={cast.name ? cast.name : "No name provided"}
+                      loading="lazy"
+                    />
+                    <ImageListItemStyle
+                      title={cast.name ? cast.name : "No name provided"}
+                      subtitle={
+                        <span>
+                          as:{" "}
+                          {cast.character
+                            ? cast.character
+                            : "No character provided"}
+                        </span>
+                      }
+                      position="below"
+                    />
+                  </ImageListItemsStyled>
+                ))}
+              </ImageListStyled>
+            </TabPanelStyled>
+            <TabPanelStyled value="2">
+              <ImageListStyled gap={0} cols={1}>
+                {castCrew?.crew?.map((crew) => (
+                  <ImageListItemsStyled key={crew.id}>
+                    <img
+                      src={
+                        crew.profile_path
+                          ? API_IMG + crew.profile_path
+                          : "https://st3.depositphotos.com/23594922/31822/v/600/depositphotos_318221368-stock-illustration-missing-picture-page-for-website.jpg"
+                      }
+                      srcSet={
+                        crew.profile_path
+                          ? API_IMG + crew.profile_path
+                          : "https://st3.depositphotos.com/23594922/31822/v/600/depositphotos_318221368-stock-illustration-missing-picture-page-for-website.jpg"
+                      }
+                      alt={crew.name ? crew.name : "No name provided"}
+                      loading="lazy"
+                    />
+                    <ImageListItemStyle
+                      title={crew.name ? crew.name : "No name provided"}
+                      subtitle={
+                        <span>
+                          as: {crew.job ? crew.job : "No job provided"}
+                        </span>
+                      }
+                      position="below"
+                    />
+                  </ImageListItemsStyled>
+                ))}
+              </ImageListStyled>
+            </TabPanelStyled>
+          </TabContext>
+        </Box>
+      </CastCrew>
   );
 }
--- src/components/CastCrew/styles.ts
+++ src/components/CastCrew/styles.ts
@@ -3,6 +3,8 @@ import Box from "@mui/material/Box";
 import ImageListItemBar from "@mui/material/ImageListItemBar";
 import ImageList from "@mui/material/ImageList";
 import ImageListItem from "@mui/material/ImageListItem";
+import TabPanel from "@mui/lab/TabPanel";
+import Tab from "@mui/material/Tab";
 
 export const CastCrew = styled(Box)`
   display: flex;
@@ -14,21 +16,37 @@ export const CastCrew = styled(Box)`
 
 export const ImageListStyled = styled(ImageList)`
   height: 270px;
-  width: calc(fit-content);
+  width: 200px;
+  overflow-x: hidden;
 `;
 
 export const ImageListItemStyle = styled(ImageListItemBar)`
   color: #f6f6f6;
   font-weight: bold;
   font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+  letter-spacing: 0.2px;
+
+
 `;
 
 export const ImageListItemsStyled = styled(ImageListItem)`
-  width: 150px;
+  width: 100%;
 
-  img{
-      max-height: 150px;
-      image-rendering: optimizeQuality;
-        object-fit: cover;
+  img {
+    max-height: 230px;
+    image-rendering: optimizeQuality;
+    object-fit: cover;
+    max-width: 200px;
   }
+`;
+
+export const TabPanelStyled = styled(TabPanel)`
+  padding: 0;
+  background: #262626;
+`;
+
+export const TabStyled = styled(Tab)`
+    flex-grow: 1;
+    color: #f6f6f6;
+    text-transform: none;
 `;
